af38000a63779758897e231d53e4f9fef79e8533
Inês Martins <inesmartins@MBP-de-Ines.Home>
2023-07-04 14:59:57 +0100

    feat: add new endpoints

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from schemas.models import Post
+
 app = FastAPI()
 
-
-my_posts = [{"title": "title of post", "content": "content of post 1", "id": 1}, 
-            {"title": "title of 2", "content": "content of post 2222", "id": 2}]
+my_posts = []
 
 
-@app.get("/")
+def find_post(id):
+    post = next((post for post in my_posts if post["id"] == id), None)
+    if post:
+        return {"data": post}
+    raise HTTPException(
+        status_code=404, detail="Post not found - check if the id is correct"
+    )
+
+
+def get_max_id_from_posts():
+    return max((post["id"] for post in my_posts), default=0)
+
+
+@app.get("/status", status_code=200)
 def root():
-    return {"message": "joaozinho noia"}
+    return {"Message": f"Server is running"}
+
 
 @app.get("/posts")
 def get_posts():
@@ -17,9 +30,22 @@ def get_posts():
 
 
 @app.post("/posts")
-def create_post(post: Post):
-    print(post.dict())
-    return {"data": post}
+async def create_post(post: Post):
+    post_dict = post.dict()
+    max_id = get_max_id_from_posts()
+    post_dict["id"] = max_id + 1
+    my_posts.append(post_dict)
+    return {"data": post_dict}
 
 
-# expected schema, title str, content str
+@app.get("/posts/latest")
+def get_latest_post():
+    id = get_max_id_from_posts()
+    data = find_post(int(id))
+    return {"data": data}
+
+
+@app.get("/posts/{id}")
+def get_post_by_id(id: int):
+    data = find_post(id)
+    return {"data": data}
