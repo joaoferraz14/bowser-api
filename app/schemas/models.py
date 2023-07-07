ac0536825bf084eb5e7117305fb1021fd427e27a
User Mac <usermac01@MBP-de-User.Home>
2023-07-07 18:03:52 +0100

    chore: refactor code and new psql adapter

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -7,4 +7,3 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True  # having the default value makes the field not necessary and defaults to true
-    rating: Optional[int] = None
