6c5f00ac9d521abdca4cd83835095f82f7808398
user <user@MBP-de-user.Home>
2024-04-11 00:21:10 +0100

    push like rambo

--- app-sqlalchemy/utils/utils.py
+++ app-sqlalchemy/utils/utils.py
@@ -2,10 +2,8 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
-
 def hash(password: str) -> str:
     return pwd_context.hash(password)
-
 
 def verify(password: str, hashed_password: str) -> bool:
     return pwd_context.verify(password, hashed_password)
